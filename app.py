8e1ec888c90407397fde7b0cc97a33c4d29def68
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-09-28 15:15:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
+import cv2
+from grabcut_processor import GrabCutProcessor
+import io
 
 # Cấu hình trang Streamlit
-st.set_page_config(layout="wide", page_title="Fixed Background Image")
+st.set_page_config(layout="wide", page_title="Deploy GrabCut")
 
-st.write("# Hình ảnh Nền Cố định")
+st.write("# Xóa Background GrabCut")
+
+st.divider()
+
+st.markdown("""
+    ## Hướng dẫn cách dùng
+    
+    * Nhấn chuột trái để vẽ hình chữ nhật.
+    * Nhấn nút 'Cập nhật' để áp dụng GrabCut.
+""")
+
+st.divider()
 
 # Sidebar để tải ảnh
 st.sidebar.write("## Upload Image")
@@ -14,28 +28,28 @@ uploaded_file = st.sidebar.file_uploader("", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     # Đọc ảnh
     image = Image.open(uploaded_file)
-    st.image(image, caption='Ảnh đầu vào', use_column_width=True)
+    image = np.array(image)
 
-    # Thêm một khung hình ẩn để xử lý sự kiện
-    st.markdown(
-        """
-        <style>
-        .fixed-image {
-            position: relative;
-            width: 100%;
-            height: auto;
-            pointer-events: none;  /* Ngăn chặn tương tác chuột */
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
+    # Khởi tạo GrabCutProcessor
+    grabcut_processor = GrabCutProcessor(image)
+
+    # Hiển thị ảnh và khung hình
+    st.image(grabcut_processor.img, caption='Ảnh đầu vào', use_column_width=True)
+
+    # Nút Cập nhật
+    if st.button("Cập nhật"):
+        grabcut_processor.apply_grabcut()
+        st.image(grabcut_processor.get_output_image(), caption='Ảnh được phân đoạn', use_column_width=True)
+
+    # Nút tải ảnh
+    output_image_pil = Image.fromarray(cv2.cvtColor(grabcut_processor.get_output_image(), cv2.COLOR_BGR2RGB))
+    buf = io.BytesIO()
+    output_image_pil.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+
+    st.download_button(
+        label="Tải ảnh xuống",
+        data=byte_im,
+        file_name="segmented_image.png",
+        mime="image/png"
     )
-
-    # Hiển thị ảnh
-    st.image(image, caption='Ảnh cố định', use_column_width=True)
-
-    # Thêm widget khác để xử lý các tương tác
-    st.button("Nút Bất Kỳ", key="some_button")
-
-    # Hiển thị thông tin hoặc điều khiển khác
-    st.write("Thông tin khác hoặc các điều khiển khác ở đây.")
