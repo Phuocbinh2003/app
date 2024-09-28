fdd6326079bf8a33c3052165ac5ff922f16f8a60
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-09-28 15:22:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,16 @@ st.divider()
 
 st.markdown("""
     ## Hướng dẫn cách dùng
-    
-    * Nhấn chuột trái để vẽ hình chữ nhật.
-    * Nhấn nút 'Cập nhật' để áp dụng GrabCut.
+
+    * Vẽ hình chữ nhật trước (vẽ bằng click phải)
+    * Sử dụng phím (chọn phím sau đó vẽ bằng click trái):
+
+        * Phím '0' - Để chọn các vùng có sure background
+        * Phím '1' - Để chọn các vùng có sure foreground
+        * Phím '2' - Để chọn các vùng probable background
+        * Phím '3' - Để chọn các vùng probable foreground
+        * Phím 'n' - Để cập nhật phân đoạn
+        * Phím 'r' - Để thiết lập lại 
 """)
 
 st.divider()
@@ -33,13 +40,19 @@ if uploaded_file is not None:
     # Khởi tạo GrabCutProcessor
     grabcut_processor = GrabCutProcessor(image)
 
-    # Hiển thị ảnh và khung hình
-    st.image(grabcut_processor.img, caption='Ảnh đầu vào', use_column_width=True)
+    # Phần xử lý ảnh
+    st.write("### Kết quả")
+    col1, col2 = st.columns([0.5, 0.5])
 
-    # Nút Cập nhật
-    if st.button("Cập nhật"):
-        grabcut_processor.apply_grabcut()
+    with col1:
+        st.image(image, caption='Ảnh đầu vào', use_column_width=True)
+
+    with col2:
         st.image(grabcut_processor.get_output_image(), caption='Ảnh được phân đoạn', use_column_width=True)
+
+    # Hiển thị vị trí chuột
+    mouse_position = st.empty()  # Tạo không gian hiển thị vị trí chuột
+    mouse_in_image = st.empty()  # Tạo không gian hiển thị trạng thái chuột
 
     # Nút tải ảnh
     output_image_pil = Image.fromarray(cv2.cvtColor(grabcut_processor.get_output_image(), cv2.COLOR_BGR2RGB))
@@ -53,3 +66,15 @@ if uploaded_file is not None:
         file_name="segmented_image.png",
         mime="image/png"
     )
+
+    # Lắng nghe sự kiện chuột
+    mouse_x = st.text_input("Mouse X:", "")
+    mouse_y = st.text_input("Mouse Y:", "")
+    
+    # Cập nhật vị trí chuột
+    if mouse_x and mouse_y:
+        mouse_position.text(f"Vị trí chuột: ({mouse_x}, {mouse_y})")
+        mouse_in_image.text("Đã vào bức ảnh")
+    else:
+        mouse_position.text("Vị trí chuột: (0, 0)")
+        mouse_in_image.text("Chưa vào bức ảnh")
