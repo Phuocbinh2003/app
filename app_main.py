073651c8618456c528e69d51ed5ef654e930d1fe
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-09-29 21:41:32 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from GrabCut_app import run_app1
-from app2 import run_app2
-from app3 import run_app3
+from app_face_detection import run_app3
+
 
 # Tạo tiêu đề cho ứng dụng chính
 st.title("Ứng dụng tổng hợp")
@@ -15,7 +15,5 @@ option = st.sidebar.selectbox(
 # Hiển thị ứng dụng tương ứng
 if option == 'Ứng dụng 1':
     run_app1()  # Gọi hàm chạy ứng dụng 1
-elif option == 'Ứng dụng 2':
-    run_app2()  # Gọi hàm chạy ứng dụng 2
 elif option == 'Ứng dụng 3':
     run_app3()  # Gọi hàm chạy ứng dụng 3
