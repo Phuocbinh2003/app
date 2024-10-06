d8fd5cc876b2622baeee837a0d03b5916f5adfb2
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-06 22:58:06 +0700

    Update app_update_firebase.py

--- app_update_firebase.py
+++ app_update_firebase.py
@@ -4,11 +4,10 @@ import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
 # Kiểm tra xem ứng dụng đã được khởi tạo chưa
-if not firebase_admin._apps:
-    cred = credentials.Certificate('phuocbinh2003-cf142-firebase-adminsdk-elr02-c3eb3c501c.json')
-    firebase_admin.initialize_app(cred, {
-        'storageBucket': 'phuocbinh2003-cf142.appspot.com'
-    })
+cred = credentials.Certificate('/content/drive/MyDrive/Mydrive/Githut/phuocbinh2003-cf142-firebase-adminsdk-elr02-7f6024b622.json')  # Thay bằng đường dẫn đến file key đúng
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'phuocbinh2003-cf142.appspot.com'  # Thêm bucket mặc định từ Firebase
+})
 
 # Khởi tạo Firestore và Storage
 db = firestore.client()
