03191c20f33f02a2be7b24ae03aad30c1acde3d3
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-06 23:54:55 +0700

    Update app_update_firebase.py

--- app_update_firebase.py
+++ app_update_firebase.py
@@ -1,17 +1,32 @@
 import streamlit as st
 import os
+import requests
+from firebase_admin import firestore, storage, credentials
 import firebase_admin
-from firebase_admin import credentials, firestore, storage
 
-# Kiểm tra xem ứng dụng đã được khởi tạo chưa
-if not firebase_admin._apps:
-    cred = credentials.Certificate('phuocbinh2003-cf142-firebase-adminsdk-elr02-c3eb3c501c.json')
-    # Khởi tạo ứng dụng với bucket storage
-    firebase_admin.initialize_app(cred, {'storageBucket': 'phuocbinh2003-cf142.appspot.com'})
+# Tải tệp JSON từ GitHub
+def download_json_from_github(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open('firebase_credentials.json', 'wb') as f:
+            f.write(response.content)
+        return 'firebase_credentials.json'
+    else:
+        raise Exception("Không thể tải tệp JSON.")
+
+# Đường dẫn tới tệp JSON trong kho GitHub của bạn
+json_url = 'https://raw.githubusercontent.com/Phuocbinh2003/app/063fc630eba23414d94b5e8f989519ae51c7c034/phuocbinh2003-cf142-firebase-adminsdk-elr02-c3eb3c501c.json'
+
+# Tải tệp JSON
+cred_path = download_json_from_github(json_url)
+cred = credentials.Certificate(cred_path)
+
+# Khởi tạo Firebase app
+firebase_admin.initialize_app(cred, {'storageBucket': 'phuocbinh2003-cf142.appspot.com'})
 
 # Khởi tạo Firestore và Storage
 db = firestore.client()
-bucket = storage.bucket()  # Lấy bucket đã được chỉ định trong quá trình khởi tạo
+bucket = storage.bucket()
 
 def upload_image(student_id, image_path, image_type):
     blob = bucket.blob(f'student-images/{student_id}/{image_type}.jpg')
