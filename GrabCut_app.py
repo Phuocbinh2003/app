152ea423e5a800ad38971e05c4161e928a0f9da0
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-06 11:07:46 +0700

    Update GrabCut_app.py

--- GrabCut_app.py
+++ GrabCut_app.py
@@ -7,7 +7,7 @@ import json
 from grabcut_processor import GrabCutProcessor
 
 
-def run_app():
+def run_app1():
     st.title("Cắt nền bằng GrabCut")
 
     # Sidebar for image upload
@@ -128,4 +128,4 @@ def convert_image_to_base64(image):
 
 # Bước 8: Chạy ứng dụng
 if __name__ == "__main__":
-    run_app()
+    run_app1()
