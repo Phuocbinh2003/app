8a7087244ebf831f74eb42b71de8585ab0c6dbaa
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-06 14:33:47 +0700

    Update grabcut_processor.py

--- grabcut_processor.py
+++ grabcut_processor.py
@@ -1,84 +1,57 @@
+import cv2 as cv
 import numpy as np
-import cv2
+
+# Constants for the mask drawing values
+DRAW_BG = {'color': [0, 0, 0], 'val': 0}       # Definite Background
+DRAW_FG = {'color': [255, 255, 255], 'val': 1}  # Definite Foreground
+DRAW_PR_BG = {'color': [128, 128, 128], 'val': 2}  # Probable Background
+DRAW_PR_FG = {'color': [192, 192, 192], 'val': 3}  # Probable Foreground
 
 class GrabCutProcessor:
-    BLUE = [255, 0, 0]        # Màu cho hình chữ nhật
-    RED = [0, 0, 255]         # Màu cho dự đoán nền
-    GREEN = [0, 255, 0]       # Màu cho dự đoán foreground
-    BLACK = [0, 0, 0]         # Màu cho nền chắc chắn
-    WHITE = [255, 255, 255]   # Màu cho foreground chắc chắn
-
-    DRAW_BG = {'color': BLACK, 'val': 0}
-    DRAW_FG = {'color': WHITE, 'val': 1}
-    DRAW_PR_BG = {'color': RED, 'val': 2}
-    DRAW_PR_FG = {'color': GREEN, 'val': 3}
-
-    def __init__(self, image):
-        self.img = image.copy()
-        self.img2 = image.copy()
-        self.mask = np.zeros(self.img.shape[:2], dtype=np.uint8)
-        self.output = np.zeros(self.img.shape, np.uint8)
+    def __init__(self, img):
+        self.img = img
+        self.img_copy = img.copy()
+        self.mask = np.zeros(self.img.shape[:2], dtype=np.uint8)  # Initial mask
         self.rect = (0, 0, 1, 1)
+        self.bgd_model = np.zeros((1, 65), np.float64)
+        self.fgd_model = np.zeros((1, 65), np.float64)
+        self.rect_or_mask = 0  # Indicates if we are using rectangle or mask
         self.drawing = False
-        self.rectangle = False
-        self.rect_over = False
-        self.value = self.DRAW_FG
+        self.value = DRAW_FG  # Default to drawing foreground
         self.thickness = 3
 
-    def clear_drawing(self):
-        # Thiết lập lại hình ảnh và mask
-        self.img = self.img2.copy()
-        self.mask = np.zeros(self.img.shape[:2], dtype=np.uint8)
-        self.output = np.zeros(self.img.shape, np.uint8)
-        self.rect = (0, 0, 1, 1)
-        self.rectangle = False
-        self.rect_over = False
+    def set_drawing_value(self, val):
+        """ Set the value to draw either background, foreground, probable background, or probable foreground """
+        if val == 0:
+            self.value = DRAW_BG
+        elif val == 1:
+            self.value = DRAW_FG
+        elif val == 2:
+            self.value = DRAW_PR_BG
+        elif val == 3:
+            self.value = DRAW_PR_FG
 
-    def onmouse(self, event, x, y, flags):
-        if event == cv2.EVENT_RBUTTONDOWN:
-            self.rectangle = True
-            self.ix, self.iy = x, y
+    def apply_grabcut(self, rect=None):
+        """ Apply the GrabCut algorithm using the rect or mask """
+        if rect:
+            self.rect = rect
+            cv.grabCut(self.img_copy, self.mask, self.rect, self.bgd_model, self.fgd_model, 5, cv.GC_INIT_WITH_RECT)
+            self.rect_or_mask = 1  # Now we are using the mask
+        else:
+            cv.grabCut(self.img_copy, self.mask, self.rect, self.bgd_model, self.fgd_model, 5, cv.GC_INIT_WITH_MASK)
 
-        elif event == cv2.EVENT_MOUSEMOVE:
-            if self.rectangle:
-                # Xoá hình đã vẽ trước đó
-                self.clear_drawing()
-                self.img = self.img2.copy()  # Khôi phục hình ảnh gốc
-                cv2.rectangle(self.img, (self.ix, self.iy), (x, y), self.BLUE, 2)
-                self.rect = (min(self.ix, x), min(self.iy, y),
-                             abs(self.ix - x), abs(self.iy - y))
+        mask2 = np.where((self.mask == 1) + (self.mask == 3), 255, 0).astype('uint8')
+        output = cv.bitwise_and(self.img_copy, self.img_copy, mask=mask2)
+        return output
 
-        elif event == cv2.EVENT_RBUTTONUP:
-            self.rectangle = False
-            self.rect_over = True
-            cv2.rectangle(self.img, (self.ix, self.iy), (x, y), self.BLUE, 2)
-            self.rect = (min(self.ix, x), min(self.iy, y),
-                         abs(self.ix - x), abs(self.iy - y))
-
-        if event == cv2.EVENT_LBUTTONDOWN:
-            self.drawing = True
-
-        elif event == cv2.EVENT_MOUSEMOVE and self.drawing:
-            cv2.circle(self.img, (x, y), self.thickness, self.value['color'], -1)
-            cv2.circle(self.mask, (x, y), self.thickness, self.value['val'], -1)
-
-        elif event == cv2.EVENT_LBUTTONUP:
-            self.drawing = False
-            cv2.circle(self.img, (x, y), self.thickness, self.value['color'], -1)
-            cv2.circle(self.mask, (x, y), self.thickness, self.value['val'], -1)
-
-    def apply_grabcut(self):
-        bgdmodel = np.zeros((1, 65), np.float64)
-        fgdmodel = np.zeros((1, 65), np.float64)
-
-        if self.rect_over:  # GrabCut với hình chữ nhật
-            cv2.grabCut(self.img2, self.mask, self.rect, bgdmodel, fgdmodel, 1, cv2.GC_INIT_WITH_RECT)
-
-        mask2 = np.where((self.mask == 1) | (self.mask == 3), 255, 0).astype('uint8')
-        self.output = cv2.bitwise_and(self.img2, self.img2, mask=mask2)
+    def update_mask(self, x, y):
+        """ Update the mask with the current drawing value """
+        cv.circle(self.img_copy, (x, y), self.thickness, self.value['color'], -1)
+        cv.circle(self.mask, (x, y), self.thickness, self.value['val'], -1)
 
     def reset(self):
-        self.clear_drawing()
-
-    def get_output_image(self):
-        return self.output
+        """ Reset the image and mask """
+        self.img_copy = self.img.copy()
+        self.mask = np.zeros(self.img.shape[:2], dtype=np.uint8)
+        self.rect_or_mask = 0
+        return self.img_copy
