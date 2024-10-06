d5446ed2ecba5484ee654a2cd08a9d8b855a2924
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-06 23:13:01 +0700

    Create firebase_config.py

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+# firebase_config.py
+import firebase_admin
+from firebase_admin import credentials
+
+# Kiểm tra xem ứng dụng đã được khởi tạo chưa
+if not firebase_admin._apps:
+    cred = credentials.Certificate('phuocbinh2003-cf142-firebase-adminsdk-elr02-c3eb3c501c.json')
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': 'phuocbinh2003-cf142.appspot.com'
+    })
