71538b8cfff060d874b58508068b374d6b18b347
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-07 09:40:57 +0700

    Update GrabCut_app.py

--- GrabCut_app.py
+++ GrabCut_app.py
@@ -64,7 +64,7 @@ def get_image_with_canvas(image, target_width=800):
             isDrawing = false;
             const endX = e.offsetX;
             const endY = e.offsetY;
-            const rectInfo = 'Hình chữ nhật: X: ' + startX + ', Y: ' + startY + ', Width: ' + (endX - startX) + ', Height: ' + (endY - startY);
+            const rectinfo = 'Hình chữ nhật: X: ' + startX + ', Y: ' + startY + ', Width: ' + (endX - startX) + ', Height: ' + (endY - startY);
             const streamlit = window.parent.document.querySelector('iframe').contentWindow;
             streamlit.document.dispatchEvent(new CustomEvent('rectangle-drawn', {{ detail: rectInfo }}));
         }});
