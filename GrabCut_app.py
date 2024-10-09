013d831b5b4826754271d8cdd1d8a17919c6ad96
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-09 11:49:48 +0700

    Update GrabCut_app.py

--- GrabCut_app.py
+++ GrabCut_app.py
@@ -76,7 +76,7 @@ def run_app1():
         rect_info = streamlit_js_eval(js_code="parent.addEventListener('message', (event) => {return event.data.rectInfo;})", key="rect_listener")
         
         if rect_info:
-            st.write(f"Thông tin hình chữ nhật: {rect_info}")
+            st.write(f"Thông tin hình chữ nhậtt: {rect_info}")
             match = re.search(r'Hình chữ nhật: X: (\d+), Y: (\d+), Width: (\d+), Height: (\d+)', rect_info)
             if match:
                 x = int(match.group(1))
