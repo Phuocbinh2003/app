c73351be052a5f208c52a05d4530b47ceada6a90
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-11 08:17:26 +0700

    Update Watershed_Segmentation.py

--- Watershed_Segmentation.py
+++ Watershed_Segmentation.py
@@ -2,6 +2,14 @@ import streamlit as st
 import cv2 as cv
 import os
 
+# Hàm để resize ảnh sao cho chiều cao của chúng bằng nhau
+def resize_image(image, target_height):
+    h, w = image.shape[:2]
+    scaling_factor = target_height / h
+    new_width = int(w * scaling_factor)
+    resized_image = cv.resize(image, (new_width, target_height))
+    return resized_image
+
 # Hàm cho ứng dụng thứ 2
 def run_app2():
     st.title('✨ Ứng dụng phân đoạn ký tự biển số ✨')
@@ -26,20 +34,22 @@ def run_app2():
         # Hiển thị ảnh "Bước thực hành" ở cột 1
         with col1:
             if os.path.exists(step_image_path):
-                img_np = cv.imread(step_image_path)
+                img_step = cv.imread(step_image_path)
 
-                if img_np is not None:
-                    st.image(img_np, caption='Bước thực hành', use_column_width=True)
+                if img_step is not None:
+                    st.image(img_step, caption='Bước thực hành', use_column_width=True)
             else:
                 st.error(f"Không tìm thấy ảnh: {step_image_path}")
 
-        # Hiển thị ảnh "Kết quả" ở cột 2
+        # Hiển thị ảnh "Kết quả" ở cột 2 với chiều cao tương ứng
         with col2:
             if os.path.exists(result_image_path):
-                img_np = cv.imread(result_image_path)
+                img_result = cv.imread(result_image_path)
 
-                if img_np is not None:
-                    st.image(img_np, caption='Kết quả', use_column_width=True)
+                if img_step is not None and img_result is not None:
+                    # Resize ảnh "Kết quả" theo chiều cao của ảnh "Bước thực hành"
+                    img_result_resized = resize_image(img_result, img_step.shape[0])
+                    st.image(img_result_resized, caption='Kết quả', use_column_width=True)
             else:
                 st.error(f"Không tìm thấy ảnh: {result_image_path}")
 
