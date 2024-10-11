550ad55db315169555895153313e6d5e0940e5df
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-11 07:59:15 +0700

    Update Watershed_Segmentation.py

--- Watershed_Segmentation.py
+++ Watershed_Segmentation.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import cv2 as cv
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-import requests
+import os
+
 # Hàm cho ứng dụng thứ 2
 def run_app2():
     st.title('✨ Ứng dụng phân đoạn ký tự biển số ✨')
 
-    # Danh sách các URL hoặc đường dẫn tới các hình ảnh train
-    train_image_urls = [
+    # Danh sách các đường dẫn tới các hình ảnh train
+    train_image_paths = [
         "my_folder/Buoc_test1.png",
         "my_folder/Buoc_test2.png",
         "my_folder/KQ_test1.png",
@@ -19,14 +17,14 @@ def run_app2():
     # Hiển thị lần lượt 4 ảnh train
     st.header("1. Ảnh Train và Kết quả")
 
-    for train_image_url in train_image_urls:
-        # Tải ảnh từ URL
-        image_response = requests.get(train_image_url)
-        nparr = np.frombuffer(image_response.content, np.uint8)
-        img_np = cv.imdecode(nparr, cv.IMREAD_COLOR)
+    for train_image_path in train_image_paths:
+        if os.path.exists(train_image_path):
+            img_np = cv.imread(train_image_path)
 
-        if img_np is not None:
-            st.image(img_np, caption='Train Image', use_column_width=True)
+            if img_np is not None:
+                st.image(img_np, caption='Train Image', use_column_width=True)
+        else:
+            st.error(f"Không tìm thấy ảnh: {train_image_path}")
 
 if __name__ == "__main__":
     run_app2()
