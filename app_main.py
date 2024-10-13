8c8df26f7f34a6a386786598ebffc5729193ba66
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-14 04:13:29 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -8,18 +8,19 @@ from App_Face_Verification import run_app5
 # Tạo thanh điều hướn
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('GrabCut', 'App2','Face_detection','Update_firebase','Face_Verification')  # Đã loại bỏ Ứng dụng 2 nếu không sử dụng
+    ('GrabCut', 'Watershed_Segmentation','Face_detection','Update_firebase','Face_Verification')  # Đã loại bỏ Ứng dụng 2 nếu không sử dụng
 )
 
 # Hiển thị ứng dụng tương ứng
 if option == 'GrabCut':
     run_app1()  # Gọi hàm chạy ứng dụng 1
-elif option == 'App2':
+elif option == 'Watershed_Segmentation':
     run_app2()  # Gọi hàm chạy ứng dụng 2
 elif option == 'Face_detection':
     run_app3()  # Gọi hàm chạy ứng dụng 3
 elif option == 'Update_firebase':
-    run_app4()  # Gọi hàm chạy ứng dụng 4
+    st.write("Ứng dụng đang fix.")
+
 elif option == 'Face_Verification':
     run_app5()  # Gọi hàm chạy ứng dụng 5
 else:
