55d3e1a351910324d69767288f507bb9b0f776b3
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-14 00:24:05 +0700

    Update App_Face_Verification.py

--- App_Face_Verification.py
+++ App_Face_Verification.py
@@ -33,11 +33,63 @@ face_recognizer = SFace(
     targetId=target_id
 )
 
+def visualize_matches(img1, faces1, img2, faces2, matches, scores, target_size=[512, 512]):  # target_size: (h, w)
+    out1 = img1.copy()
+    out2 = img2.copy()
+    matched_box_color = (0, 255, 0)  # BGR
+    mismatched_box_color = (0, 0, 255)  # BGR
+
+    # Resize to target size
+    padded_out1 = np.zeros((target_size[0], target_size[1], 3)).astype(np.uint8)
+    h1, w1, _ = out1.shape
+    ratio1 = min(target_size[0] / out1.shape[0], target_size[1] / out1.shape[1])
+    new_h1 = int(h1 * ratio1)
+    new_w1 = int(w1 * ratio1)
+    resized_out1 = cv2.resize(out1, (new_w1, new_h1), interpolation=cv2.INTER_LINEAR).astype(np.float32)
+    top = max(0, target_size[0] - new_h1) // 2
+    bottom = top + new_h1
+    left = max(0, target_size[1] - new_w1) // 2
+    right = left + new_w1
+    padded_out1[top:bottom, left:right] = resized_out1
+
+    # Draw bbox
+    bbox1 = faces1[0][:4] * ratio1
+    x, y, w, h = bbox1.astype(np.int32)
+    cv2.rectangle(padded_out1, (x + left, y + top), (x + left + w, y + top + h), matched_box_color, 2)
+
+    # Resize to target size
+    padded_out2 = np.zeros((target_size[0], target_size[1], 3)).astype(np.uint8)
+    h2, w2, _ = out2.shape
+    ratio2 = min(target_size[0] / out2.shape[0], target_size[1] / out2.shape[1])
+    new_h2 = int(h2 * ratio2)
+    new_w2 = int(w2 * ratio2)
+    resized_out2 = cv2.resize(out2, (new_w2, new_h2), interpolation=cv2.INTER_LINEAR).astype(np.float32)
+    top = max(0, target_size[0] - new_h2) // 2
+    bottom = top + new_h2
+    left = max(0, target_size[1] - new_w2) // 2
+    right = left + new_w2
+    padded_out2[top:bottom, left:right] = resized_out2
+
+    # Draw bbox
+    assert faces2.shape[0] == len(matches), "number of faces2 needs to match matches"
+    assert len(matches) == len(scores), "number of matches needs to match number of scores"
+    for index, match in enumerate(matches):
+        bbox2 = faces2[index][:4] * ratio2
+        x, y, w, h = bbox2.astype(np.int32)
+        box_color = matched_box_color if match else mismatched_box_color
+        cv2.rectangle(padded_out2, (x + left, y + top), (x + left + w, y + top + h), box_color, 2)
+
+        score = scores[index]
+        text_color = matched_box_color if match else mismatched_box_color
+        cv2.putText(padded_out2, "{:.2f}".format(score), (x + left, y + top - 5), cv2.FONT_HERSHEY_DUPLEX, 0.4, text_color)
+
+    return np.concatenate([padded_out1, padded_out2], axis=1)
+
 def find_similar_faces(uploaded_image, folder_path):
     results = []
-    # Chuyển đổi hình ảnh từ file uploader thành mảng NumPy
-    image1 = Image.open(uploaded_image).convert("RGB")  # Chuyển đổi sang RGB
-    image1 = cv2.cvtColor(np.array(image1), cv2.COLOR_RGB2BGR)  # Chuyển đổi sang BGR
+    # Convert uploaded image to NumPy array
+    image1 = Image.open(uploaded_image).convert("RGB")  # Convert to RGB
+    image1 = cv2.cvtColor(np.array(image1), cv2.COLOR_RGB2BGR)  # Convert to BGR
 
     face_detector.setInputSize([image1.shape[1], image1.shape[0]])
     faces1 = face_detector.infer(image1)
@@ -49,7 +101,7 @@ def find_similar_faces(uploaded_image, folder_path):
     for filename in os.listdir(folder_path):
         img_path = os.path.join(folder_path, filename)
         image2 = cv2.imread(img_path)
-        if image2 is not None:  # Kiểm tra nếu ảnh được đọc thành công
+        if image2 is not None:  # Check if image was read successfully
             face_detector.setInputSize([image2.shape[1], image2.shape[0]])
             faces2 = face_detector.infer(image2)
 
@@ -58,6 +110,47 @@ def find_similar_faces(uploaded_image, folder_path):
                 results.append((filename, result[0], result[1]))
 
     return results
+
+def visualize_faces(image, results, box_color=(0, 255, 0), text_color=(0, 0, 255), fps=None):
+    output = image.copy()
+    landmark_color = [
+        (255, 0, 0),   # right eye
+        (0, 0, 255),   # left eye
+        (0, 255, 0),   # nose tip
+        (255, 0, 255), # right mouth corner
+        (0, 255, 255)  # left mouth corner
+    ]
+
+    if fps is not None:
+        cv2.putText(output, 'FPS: {:.2f}'.format(fps), (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color)
+
+    for det in results:
+        bbox = det[0:4].astype(np.int32)
+        cv2.rectangle(output, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), box_color, 2)
+
+        conf = det[-1]
+        cv2.putText(output, '{:.4f}'.format(conf), (bbox[0], bbox[1]+12), cv2.FONT_HERSHEY_DUPLEX, 0.5, text_color)
+
+        landmarks = det[4:14].astype(np.int32).reshape((5, 2))
+        for idx, landmark in enumerate(landmarks):
+            cv2.circle(output, landmark, 2, landmark_color[idx], 2)
+
+    return output
+
+def resize_image(image, target_size=320):
+    h, w, _ = image.shape
+    # Check which dimension is larger
+    if w > h:
+        new_w = target_size
+        new_h = int(h * (target_size / w))  # Calculate height based on width
+    else:
+        new_h = target_size
+        new_w = int(w * (target_size / h))  # Calculate width based on height
+
+    # Resize image while keeping aspect ratio
+    resized_image = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
+
+    return resized_image
 
 def read_student_info(filename, folder_path):
     txt_file_path = os.path.join(folder_path, f"{os.path.splitext(filename)[0]}.txt")
@@ -78,15 +171,20 @@ def run_app5():
         similar_faces = find_similar_faces(uploaded_file, folder_path)
 
         if similar_faces:
-            # Tìm file có độ chính xác cao nhất
-            best_match = max(similar_faces, key=lambda x: x[1])  # x[1] là độ chính xác
-            best_filename = best_match[0]
-            best_score = best_match[1]
+            # Find the file with the highest accuracy
+            best_match = max(similar_faces, key=lambda x: x[1])  # x[1] is the accuracy
+            st.write(f"Best match: {best_match[0]} with score: {best_match[1]:.4f}")
 
-            st.write(f"File with highest accuracy: {best_filename}, Score: {best_score:.4f}")
-            
-            # Đọc file txt tương ứng
-            student_info = read_student_info(best_filename, folder_path)
+            # Display results for all similar faces
+            for filename, score, _ in similar_faces:
+                st.write(f"{filename}: Score: {score:.4f}")
+
+            # Display the best match
+            img_best_match = Image.open(os.path.join(folder_path, best_match[0]))
+            st.image(img_best_match, caption="Best Match Image", use_column_width=True)
+
+            # Read student info
+            student_info = read_student_info(best_match[0], folder_path)
             st.write("Student Information:")
             st.write(student_info)
         else:
