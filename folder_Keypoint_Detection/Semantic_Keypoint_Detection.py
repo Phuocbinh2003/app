a0f497cf7c2630567fb51d6380f16740a3612881
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-26 21:20:07 +0700

    Update Semantic_Keypoint_Detection.py

--- folder_Keypoint_Detection/Semantic_Keypoint_Detection.py
+++ folder_Keypoint_Detection/Semantic_Keypoint_Detection.py
@@ -45,7 +45,7 @@ def run_app6():
     data = {
         "Method": ["SIFT", "ORB"],
         "Precision": [0.2168, 0.2419],  # Dữ liệu mẫu
-        "Recall": [0.3145, 0.3014]      # Dữ liệu mẫu
+        "Recall": [0.3145, 0.3014]      # Dữ liệu mẫ
     }
     
     df = pd.DataFrame(data)
