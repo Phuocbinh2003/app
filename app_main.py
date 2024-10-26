c56206e6b5a265985dacd7938d0da84756f7c0af
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-10-26 19:30:28 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -4,11 +4,11 @@ from Watershed_Segmentation import run_app2  # Đã sửa tên module không có
 from app_face_detection import run_app3
 from app_update_firebase import run_app4
 from App_Face_Verification import run_app5
-
+from folder_Keypoint_Detection.Semantic_Keypoint_Detection import run_app6
 # Tạo thanh điều hướn
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('GrabCut', 'Watershed_Segmentation','Face_detection','Update_firebase','Face_Verification')  # Đã loại bỏ Ứng dụng 2 nếu không sử dụng
+    ('GrabCut', 'Watershed_Segmentation','Face_detection','Update_firebase','Face_Verification','app_6')  # Đã loại bỏ Ứng dụng 2 nếu không sử dụng
 )
 
 # Hiển thị ứng dụng tương ứng
@@ -23,5 +23,7 @@ elif option == 'Update_firebase':
 
 elif option == 'Face_Verification':
     run_app5()  # Gọi hàm chạy ứng dụng 5
+elif option == 'app_6':
+    run_app6()  # Gọi hàm chạy ứng dụng 5
 else:
     st.write("Vui lòng chọn một ứng dụng từ thanh điều hướng.")
