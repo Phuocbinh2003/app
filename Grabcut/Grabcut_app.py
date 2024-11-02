73ec2f165c259008e2b1a631f8b07f29a5b0af86
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-11-02 14:28:19 +0700

    Update Grabcut_app.py

--- Grabcut/Grabcut_app.py
+++ Grabcut/Grabcut_app.py
@@ -11,11 +11,9 @@ from Grabcut.ultis import get_object_from_st_canvas
 
 init_session_state()
 
-
 def run_app1():
     st.set_page_config(
         page_title="Ứng dụng tách nền bằng thuật toán GrabCut",
-        page_icon=Image.open("./public/images/logo.png"),
         layout="wide",
         initial_sidebar_state="expanded",
     )
@@ -43,5 +41,6 @@ def run_app1():
             with cols[1]:
                 result = process_grabcut(raw_image, canvas_result, rects, true_fgs, true_bgs)
                 st.image(result, caption="Ảnh sau khi tách nền", use_column_width="always")
+
 if __name__ == "__main__":
     run_app1()
