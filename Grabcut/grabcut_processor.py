5210458cad6f6e1bb05a1ff730b4409297d4f3c8
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-11-02 14:25:46 +0700

    Update grabcut_processor.py

--- Grabcut/grabcut_processor.py
+++ Grabcut/grabcut_processor.py
@@ -4,7 +4,7 @@ from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas, CanvasResult
 
-from Grabcut.Grabcut import grabcut
+from Grabcut.grabcut import grabcut
 
 # Khởi tạo trạng thái
 def init_session_state():
