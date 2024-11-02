b7b5984e24981cd86f453688e84c181dc1fbef4c
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-11-02 14:41:36 +0700

    Update Grabcut_app.py

--- Grabcut/Grabcut_app.py
+++ Grabcut/Grabcut_app.py
@@ -24,26 +24,24 @@ def run_app1():
     with st.container():
         display_guide()
     
-    # Tải ảnh lên
     uploaded_image = st.file_uploader("Chọn hoặc kéo ảnh vào ô bên dưới", type=["jpg", "jpeg", "png"])
     
     if uploaded_image is not None:
-        # Mở ảnh và chuyển sang định dạng RGB để đảm bảo màu sắc chính xác
-        raw_image = Image.open(uploaded_image).convert("RGB")
-        
-        # Thiết lập độ dày nét vẽ mặc định là 3 trong hàm display_form_draw()
         drawing_mode, stroke_width = display_form_draw()
-        
-        # Hiển thị canvas với ảnh đã tải lên và đặt nét vẽ mặc định là 3
-        canvas_result = display_st_canvas(raw_image, drawing_mode, stroke_width=3)
-        
-        # Lấy các đối tượng từ canvas
-        rects, true_fgs, true_bgs = get_object_from_st_canvas(canvas_result)
+        cols = st.columns(2, gap="large")
+        raw_image = Image.open(uploaded_image)
     
-        # Xử lý tách nền và hiển thị ảnh kết quả
-        if len(rects) > 0:
-            result = process_grabcut(raw_image, canvas_result, rects, true_fgs, true_bgs)
-            st.image(result, caption="Ảnh sau khi tách nền", use_column_width=True)
+        with cols[0]:
+            canvas_result = display_st_canvas(raw_image, drawing_mode, stroke_width)
+            rects, true_fgs, true_bgs = get_object_from_st_canvas(canvas_result)
+    
+        if len(rects) < 1:
+            st.session_state["result_grabcut"] = None
+            st.session_state["final_mask"] = None
+        elif len(rects) > 0:
+            with cols[1]:
+                result = process_grabcut(raw_image, canvas_result, rects, true_fgs, true_bgs)
+                st.image(result, caption="Ảnh sau khi tách nền", use_column_width="always")
 
 if __name__ == "__main__":
     run_app1()
