7ba220687c7cdb44e3bc1f981a51dc53b4b18df2
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-11-02 16:20:29 +0700

    Update Grabcut_app.py

--- Grabcut/Grabcut_app.py
+++ Grabcut/Grabcut_app.py
@@ -28,18 +28,10 @@ def run_app1():
             ":material/image: Chọn hoặc kéo ảnh vào ô bên dưới", type=["jpg", "jpeg", "png"]
         )
     
-    # Giữ chế độ vẽ đã chọn và độ dày nét vẽ
-    if 'drawing_mode' not in st.session_state:
-        st.session_state['drawing_mode'] = "rect"  # Chế độ vẽ mặc định là hình chữ nhật
-
-    if 'stroke_width' not in st.session_state:
-        st.session_state['stroke_width'] = 3  # Độ dày nét vẽ mặc định là 3
-
     if uploaded_image is not None:
         with st.container(border=True):
-            drawing_mode = st.session_state['drawing_mode']
-            stroke_width = st.session_state['stroke_width']
-
+            drawing_mode, stroke_width = display_form_draw()
+    
         with st.container(border=True):
             cols = st.columns(2, gap="large")
             raw_image = Image.open(uploaded_image)
@@ -47,20 +39,12 @@ def run_app1():
             with cols[0]:
                 canvas_result = display_st_canvas(raw_image, drawing_mode, stroke_width)
                 rects, true_fgs, true_bgs = get_object_from_st_canvas(canvas_result)
-
-                # Cập nhật trạng thái nếu đã vẽ hình chữ nhật
-                if len(rects) > 0:
-                    st.session_state["rect_drawn"] = True
-                else:
-                    st.session_state["rect_drawn"] = False
-
-            # Reset trạng thái khi không có hình chữ nhật nào được vẽ
+    
             if len(rects) < 1:
                 st.session_state["result_grabcut"] = None
                 st.session_state["final_mask"] = None
-                st.session_state["rect_drawn"] = False
             elif len(rects) > 1:
-                st.warning("Chỉ được chọn một vùng cần tách nền.")
+                st.warning("Chỉ được chọn một vùng cần tách nền")
             else:
                 with cols[0]:
                     submit_btn = st.button(
@@ -68,14 +52,11 @@ def run_app1():
                     )
     
                 if submit_btn:
-                    if len(rects) > 1:
-                        st.warning("Chỉ được chọn một hình chữ nhật để tách nền.")
-                    else:
-                        with st.spinner("Đang xử lý..."):
-                            result = process_grabcut(
-                                raw_image, canvas_result, rects, true_fgs, true_bgs
-                            )
-                            cols[1].image(result, channels="BGR", caption="Ảnh kết quả")
+                    with st.spinner("Đang xử lý..."):
+                        result = process_grabcut(
+                            raw_image, canvas_result, rects, true_fgs, true_bgs
+                        )
+                        cols[1].image(result, channels="BGR", caption="Ảnh kết quả")
                 elif st.session_state["result_grabcut"] is not None:
                     cols[1].image(
                         st.session_state["result_grabcut"],
@@ -84,4 +65,4 @@ def run_app1():
                     )
 
 if __name__ == "__main__":
-    run_app1()
+    run_app1() 
