fb6666f2df2e05ab13e2f98496e000b2bc634070
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-11-24 21:36:45 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -8,10 +8,11 @@ from folder_Keypoint_Detection.Semantic_Keypoint_Detection import run_app6
 from folder_SuperPoint_Rotation.app_superpoint_rotation import run_app7
 from truy_van.Instance_Search import run_app8
 from foder_Object_racking.Object_Tracking_app import run_app9
+from MOT-SORT.mot_sort import run_app10
 # Tạo thanh điều hướn
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('GrabCut', 'Watershed_Segmentation','Face_detection','Face_Verification','Semantic_Keypoint_Detection','SuperPoint_Rotation','Instance_Search'
+    ('GrabCut', 'Watershed_Segmentation','Face_detection','Face_Verification','Semantic_Keypoint_Detection','SuperPoint_Rotation','Instance_Search','Thuật toán SORT'
     ,'OpenCV_Object_Tracking')  # Đã loại bỏ Ứng dụng 2 nếu không sử dụng
 )
 
@@ -35,5 +36,7 @@ elif option == 'Instance_Search':
     run_app8()  # Gọi hàm chạy ứng dụng 
 elif option == 'OpenCV_Object_Tracking':
     run_app9()  # Gọi hàm chạy ứng dụng 
+elif option == 'Thuật toán SORT':
+    run_app10()  # Gọi hàm chạy ứng dụng    
 else:
     st.write("Vui lòng chọn một ứng dụng từ thanh điều hướng.")
