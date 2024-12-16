3c5d4d03c4fe46da29c507ad1b707e13ad65fc8f
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-12-16 07:09:47 +0700

    Update app_handwriting.py

--- Handwriting_Letter_Recognition/app_handwriting.py
+++ Handwriting_Letter_Recognition/app_handwriting.py
@@ -5,9 +5,8 @@ from tensorflow.keras.datasets import mnist
 
 
 def run_app11():
-  # Tiêu đề phần 1
   st.header("Phần 1: MNIST Dataset")
-
+  
   # Mô tả về bộ dữ liệu
   st.write(
       """
@@ -28,18 +27,23 @@ def run_app11():
   # Tải dữ liệu MNIST
   (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
   
-  # Hiển thị một lưới 9x9 ảnh từ bộ dữ liệu
+  # Hiển thị một lưới 9x9 ảnh từ bộ dữ liệu, xếp theo hàng từ 0 đến 9
   st.subheader("Lưới 9x9 hình ảnh từ MNIST Dataset")
   fig, axes = plt.subplots(9, 9, figsize=(10, 10))
-  indices = np.random.choice(len(train_images), size=81, replace=False)
   
-  for i, ax in enumerate(axes.flatten()):
-      ax.imshow(train_images[indices[i]], cmap='gray')
-      ax.axis('off')
-      ax.set_title(str(train_labels[indices[i]]), fontsize=8)
+  for row in range(9):
+      count = 0
+      for i in range(len(train_images)):
+          if train_labels[i] == row:
+              ax = axes[row, count]
+              ax.imshow(train_images[i], cmap='gray')
+              ax.axis('off')
+              ax.set_title(str(train_labels[i]), fontsize=8)
+              count += 1
+              if count == 9:
+                  break
   
   plt.tight_layout()
   st.pyplot(fig)
-
 if __name__ == "__main__":
     run_app11()
